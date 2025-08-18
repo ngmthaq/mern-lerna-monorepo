61c366229d53b4a202cf9d6e582be9894a475ac1
ngmthaq <ngmthaq.20@gmail.com>
2025-08-18 14:55:55 +0700

    feat: add lint in nest-app

--- packages/nest-app/eslint.config.mjs
+++ packages/nest-app/eslint.config.mjs
@@ -1,34 +1,71 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["eslint.config.mjs"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': ['warn', {}],
-      '@typescript-eslint/no-floating-promises': ['warn', {}],
-      '@typescript-eslint/no-unsafe-argument': ['warn', {}],
+      "no-unused-vars": ["warn", {}],
+      "@typescript-eslint/no-explicit-any": ["warn", {}],
+      "@typescript-eslint/no-floating-promises": ["warn", {}],
+      "@typescript-eslint/no-unsafe-argument": ["warn", {}],
+      "import/no-unresolved": ["warn", {}],
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          groups: [
+            "type",
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "unknown",
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+        },
+      ],
     },
   },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 );
