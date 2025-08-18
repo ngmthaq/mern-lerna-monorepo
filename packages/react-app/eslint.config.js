16c31c0dabbb5b72add9eadb95afcee7e49007bb
ngmthaq <ngmthaq.20@gmail.com>
2025-08-18 14:48:44 +0700

    feat: add lint for react-app

--- packages/react-app/eslint.config.js
+++ packages/react-app/eslint.config.js
@@ -1,27 +1,68 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    parserOptions: {
-      project: './tsconfig.json',
-      tsconfigRootDir: __dirname,
-    },
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", {}],
+      "import/no-dynamic-require": ["warn", {}],
+      "import/no-nodejs-modules": ["warn", {}],
+      "import/no-unresolved": ["warn", {}],
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          groups: [
+            "type",
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "unknown",
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+        },
+      ],
     },
   },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 ]);
