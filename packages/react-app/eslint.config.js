f81d33c3db2bfd3bea7b3eef817149cca2129896
ngmthaq <ngmthaq.20@gmail.com>
2025-08-18 12:00:34 +0700

    feat: add prettier

--- packages/react-app/eslint.config.js
+++ packages/react-app/eslint.config.js
@@ -1,13 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+    },
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
@@ -20,4 +24,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+]);
